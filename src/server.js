c6e9b3807ffc1f3ac2875c256b19c79660f059f5
Oleksandra Ovcharova <oleksaovcharova@gmail.com>
2025-10-19 19:07:51 +0200

    Homework

--- src/server.js
+++ src/server.js
@@ -16,10 +16,6 @@ app.use(logger);
 
 app.use(notesRoutes);
 
-app.get('/test-error', () => {
-  throw new Error('Simulated server error');
-});
-
 app.use(notFoundHandler);
 
 app.use(errors());
--- src/validations/notesValidation.js
+++ src/validations/notesValidation.js
@@ -34,7 +34,7 @@ export const updateNoteSchema = {
 export const getAllNotesSchema = {
   [Segments.QUERY]: Joi.object({
     page: Joi.number().integer().min(1).default(1),
-    perPage: Joi.number().integer().min(5).max(20).default(5),
+    perPage: Joi.number().integer().min(5).max(20).default(10),
     tag: Joi.string().valid(...TAGS),
     search: Joi.string().trim().allow(''),
   }),

7c53549fd48c1ab7f45bb7a18b2d614d5df991dc
Oleksandra Ovcharova <oleksaovcharova@gmail.com>
2025-10-28 09:27:01 +0100

    Homework

--- src/server.js
+++ src/server.js
@@ -1,19 +1,23 @@
 import express from 'express';
 import 'dotenv/config';
 import cors from 'cors';
+import cookieParser from 'cookie-parser';
 import { connectMongoDB } from './db/connectMongoDB.js';
 import { logger } from './middleware/logger.js';
 import { notFoundHandler } from './middleware/notFoundHandler.js';
 import { errors } from 'celebrate';
 import { errorHandler } from './middleware/errorHandler.js';
+import authRoutes from './routes/authRoutes.js';
 import notesRoutes from './routes/notesRoutes.js';
 
 const app = express();
 
 app.use(express.json());
 app.use(cors());
+app.use(cookieParser());
 app.use(logger);
 
+app.use(authRoutes);
 app.use(notesRoutes);
 
 app.use(notFoundHandler);
